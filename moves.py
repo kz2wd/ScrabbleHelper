4daf50c1470f65dc2d5ddeefbd9b29658a153dcc
kz2wd <yrus.chan@gmail.com>
2024-09-14 23:03:06 +0200

    Added best of all words

--- moves.py
+++ moves.py
@@ -23,16 +23,26 @@ class ScrabbleMoveGenerator:
         return possible_words
 
 
+def filter_longest_words(moves):
+    if not moves: return []
+    words_with_length = [(len(word), word) for word in moves]
+    best_length, _ = max(words_with_length)
+    best_words = list(map(operator.itemgetter(1), filter(lambda x: x[0] == best_length, words_with_length)))
+    return best_words
+
+
 if __name__ == '__main__':
     move_generator = ScrabbleMoveGenerator(Dictionary('dictionary.csv'))
 
-
+    all_words = set()
     alphabet = string.ascii_lowercase
     for letter in alphabet:
         moves = move_generator.generate_moves("lwgpyia" + letter, None)
-        if not moves: continue
-        words_with_length = [(len(word), word) for word in moves]
-        best_length, _ = max(words_with_length)
-        best_words = list(map(operator.itemgetter(1), filter(lambda x: x[0] == best_length, words_with_length)))
+        best_words = filter_longest_words(moves)
+
+        all_words.update(best_words)
         print(f"{letter} : " + ", ".join(best_words))
 
+    best_of_all = filter_longest_words(all_words)
+    print("BEST OF ALL : ")
+    print(best_of_all)
